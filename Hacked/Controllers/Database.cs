d98b6319aaa5baa3f56e92a22451ad8bb00995ef
Bryan Eggels <356103@student.fontys.nl>
2017-04-04 22:33:36 +0200

    initial commit

---
+++ Hacked/Controllers/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace Hacked.Controllers
+{
+    class Database
+    {
+        public static SqlConnection Connection
+        {
+            get
+            {
+                SqlConnection connection = new SqlConnection(connectionstring);
+                connection.Open();
+                return connection;
+            }
+        }
+        //!!!UPDATE CONNECTIONSTRING!!!
+        private static string connectionstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"c:\\users\\fam_e\\onedrive\\documenten\\visual studio 2015\\Projects\\Hacked\\Hacked\\App_Data\\Database.mdf\";Integrated Security = True";
+    }
+}
---
+++ Hacked/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace Hacked.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string email, string password)
+        {
+            using(SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"c:\\users\\fam_e\\onedrive\\documenten\\visual studio 2015\\Projects\\Hacked\\Hacked\\App_Data\\Database.mdf\";Integrated Security = True"))
+            {
+                con.Open();
+                string query = "SELECT Email FROM [User] WHERE Email = '" + email + "' AND Password = '" + password + "'";
+                SqlCommand cmd = new SqlCommand(query, con);
+                if(cmd.ExecuteScalar() != null)
+                {
+                    Session["User"] = email;
+                }
+                return RedirectToAction("Index", "Home");
+
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
